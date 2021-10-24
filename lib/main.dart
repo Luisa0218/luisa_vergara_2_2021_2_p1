90151c09d9269abdfc389ce7949c91a2e206dc91
Luisa0218 <78153272+Luisa0218@users.noreply.github.com>
2021-10-24 02:22:54 -0500

    Add_Main

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,18 @@
+// @dart=2.9
 
+import 'package:elephant_app/screens/elephants_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+// ignore: use_key_in_widget_constructors
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Elephant App',
+      home: ElephantsScreen(),
+    );
+  }
+}
